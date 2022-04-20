3ee20e20c407968e55233c2f67d03f5312de4986
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-04-20 08:44:15 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,203 @@
+<script>
+	import Dialog from './lib/Dialog.svelte';
+	import { onMount } from 'svelte';
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
+	import { easeElastic3 } from './lib/easing';
+	
+	let megaDialog, miniDialog;
+	let id = 1;
+	let topOffset, leftOffset;
+	let toRemove;
+	let fileInput;
+	
+	function openMegaDialog() {
+		megaDialog.open();
+	}
+	
+	function promptRemove(e, id) {
+		const removeButton = e.target;
+		const bounds = removeButton.getBoundingClientRect();
+		topOffset = bounds.y;
+		leftOffset = bounds.left;
+		miniDialog.open();
+		toRemove = id;
+	}
+	
+	function removeUser({ detail }) {
+		if (detail.returnValue === 'confirm') {
+			const index = users.findIndex(u => u.id === toRemove);
+			users.splice(index, 1);
+			users = users;
+		}
+	}
+	
+	function handleFile({ detail }) {
+		let files = fileInput.files;
+		if (detail.returnValue === 'confirm' && files.length > 0 && files[0].size) {			
+			const reader = new FileReader();
+			reader.addEventListener('load', e => {
+				addUser(e.target.result);
+			}, false)
+			reader.readAsDataURL(files[0]);
+			fileInput.form.reset();
+		}
+	}
+	
+	function addUser(src) {
+		users.push({ src, id: id++ });
+		users = users;
+	}
+	
+	let users = [
+		{ src: 'https://doodleipsum.com/700x700/avatar-5?i=e49dd48962488beade4c44491e56a5a9', id: id++ },
+		{ src: 'https://doodleipsum.com/700x700/avatar-5?i=ca71b901f3ad2ce77d50ab296aff3f5f', id: id++ },
+		{ src: 'https://doodleipsum.com/700x700/avatar-5?i=ca71b901f3ad2ce77d50ab296aff3f5f', id: id++ },
+		{ src: 'https://doodleipsum.com/700x700/avatar-5?i=ca71b901f3ad2ce77d50ab296aff3f5f', id: id++ }
+	];
+</script>
+
+<Dialog mode="mega" bind:this={megaDialog} on:closed={handleFile}>
+	<section class="icon-headline" slot="header">
+		<svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
+			<path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+			<circle cx="8.5" cy="7" r="4"></circle>
+			<line x1="20" y1="8" x2="20" y2="14"></line>
+			<line x1="23" y1="11" x2="17" y2="11"></line>
+		</svg>
+		<h3>New User</h3>
+	</section>
+	<svelte:fragment slot="content">
+		<section class="labelled-input">
+			<label for="userimage">Upload an image</label>
+			<input id="userimage" name="userimage" type="file" bind:this={fileInput}>
+		</section>
+		<small><b>*</b> Maximum upload 1mb</small>
+	</svelte:fragment>
+	<menu slot="menu">
+		<button type="reset" value="clear">Clear</button>
+	</menu>
+</Dialog>
+
+<Dialog mode="mini" bind:this={miniDialog} {topOffset} {leftOffset} on:closing={removeUser}>
+	<svelte:fragment slot="content">
+		<section class="warning-message">
+			<svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" >
+				<title>A warning icon</title>
+				<path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+				<line x1="12" y1="9" x2="12" y2="13"></line>
+				<line x1="12" y1="17" x2="12.01" y2="17"></line>
+			</svg>
+			<p>Are you sure you want to remove this user?</p>
+		</section>
+	</svelte:fragment>
+</Dialog>
+
+<main>
+	<!-- Add an empty user to the end that will act as a button
+			This way we can apply FLIP to the button as well -->
+	{#each [...users, { id: -1 }] as user (user.id)}
+		<div animate:flip={{duration: 400}}>
+			{#if user.src}
+				<div class="user" in:fly={{y: -120, easing: easeElastic3 }}>
+					<img src={user.src} alt="">
+					<button title="Remove user" on:click={(e) => promptRemove(e, user.id)}>
+						<svg width="24" height="24" viewBox="0 0 24 24">
+							<line x1="18" y1="6" x2="6" y2="18"/>
+							<line x1="6" y1="6" x2="18" y2="18"/>
+						</svg>
+					</button>
+				</div>
+			{:else}
+				<button class="user" on:click={openMegaDialog}>
+					<svg width="24" height="24" viewBox="0 0 24 24">
+						<line x1="12" y1="5" x2="12" y2="19"></line>
+						<line x1="5" y1="12" x2="19" y2="12"></line>
+					</svg>
+				</button>
+			{/if}
+		</div>
+	{/each}
+</main>
+
+<style>
+	:global(body) {
+		display: grid;
+		place-content: center;
+		padding-inline: var(--size-5);
+	}
+	
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		place-content: center;
+		gap: var(--size-5);
+	}
+
+	.user {
+		inline-size: var(--size-fluid-7);
+		aspect-ratio: var(--ratio-square);
+		border-radius: var(--radius-round);
+		border: var(--border-size-1) solid var(--surface-4);
+		display: flex;
+		align-items: stretch;
+		justify-content: stretch;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+	}
+	
+	button.user > svg {
+		--_icon-size: var(--size-fluid-4);
+	}
+	
+	.user > button {
+		position: absolute;
+		inset-block-start: 0;
+		inset-inline-end: 0;
+		border-radius: var(--radius-round);
+		padding: .75ch;
+		aspect-ratio: 1;
+		flex-shrink: 0;
+	}
+	
+	.user img {
+		border-radius: inherit;
+	}
+
+	.labelled-input {
+		display: flex;
+		gap: var(--size-3);
+		flex-wrap: wrap;
+		align-items: center;
+	}
+
+	.warning-message {
+		display: grid;
+		grid-template-columns: var(--size-fluid-3) 1fr;
+		gap: var(--size-3);
+		align-items: center;
+	}
+	
+	.warning-message > svg {
+		inline-size: 100%;
+		stroke: var(--text-2);
+	}
+
+	.icon-headline {
+		display: flex;
+		gap: var(--size-3);
+		align-items: center;
+	}
+	
+	.icon-headline > svg {
+		inline-size: 2em;
+		stroke: var(--text-2);
+	}
+
+	@media (max-width: 768px) {
+		input[type="file"] {
+			inline-size: 100%;
+		}
+	}
+</style>
---
+++ src/lib/Dialog.svelte
@@ -0,0 +1,265 @@
+<script>
+	import { onMount, createEventDispatcher } from 'svelte';
+	
+	export let mode = 'mega';
+	export let topOffset = -1;
+	export let leftOffset = -1;
+	
+	const dispatch = createEventDispatcher();
+	
+	let dialog;
+	let loading = true;
+	let isOpen = false;
+	let marginTop = null;
+	let marginLeft = null;
+	
+	onMount(() => { loading = false; });
+	
+	const animationsComplete = element =>
+		Promise.allSettled(
+			element.getAnimations().map(animation => 
+				animation.finished));
+	
+	function close() {
+		dialog.close('close');
+	}
+	
+	async function closeDialog() {
+		isOpen = false;
+		let { returnValue } = dialog;
+		dispatch('closing', { returnValue });
+		await animationsComplete(dialog);
+		dispatch('closed', { returnValue });
+	}
+	
+	export function open() {
+		dialog.showModal();
+		isOpen = true;
+	}
+	
+	function focus(node) {
+		node.focus();
+	}
+	
+	$: updateMargin(topOffset, leftOffset);
+	
+	function updateMargin(topOffset, leftOffset) {
+		if (topOffset >= 0 && leftOffset >= 0) {
+			const miniModalHeight = dialog.clientHeight - 15;
+			const miniModalWidth = dialog.clientWidth / 2;
+			
+			let left = leftOffset - miniModalWidth;
+			if (left < 0) left = 10;
+			marginTop = topOffset - miniModalHeight + 'px';
+			marginLeft = window.innerWidth >= 768 ? left + 'px' : null;
+		}
+	}
+</script>
+
+<dialog 
+				inert={isOpen ? undefined : ''} 
+				loading={loading ? '' : undefined} 
+				modal-mode={mode} 
+				bind:this={dialog}
+				style:margin-top={marginTop}
+				style:margin-left={marginLeft}
+				on:close={closeDialog}
+				> 
+	<form method="dialog">
+		{#if mode === 'mega'}
+		<header> 
+			<slot name="header"></slot>
+			<button on:click={close} type="button" title="Close dialog"> 
+				<title>Close dialog icon</title>
+				<svg width="24" height="24" viewBox="0 0 24 24">
+					<line x1="18" y1="6" x2="6" y2="18"/>
+					<line x1="6" y1="6" x2="18" y2="18"/>
+				</svg>
+			</button>
+		</header>
+		{/if}
+		<article>
+			<slot name="content"></slot>
+		</article>
+		<footer>
+			<slot name="menu"></slot>
+			<menu>
+				<!-- TODO: focus doesn't seem to be working -->
+				<button use:focus type="button" on:click={close}>Cancel</button>
+				<button type="submit" value="confirm">Confirm</button>
+			</menu>
+		</footer>
+	</form>
+</dialog>
+
+<style>
+	:global(html):has(dialog[open][modal-mode="mega"]) {
+		overflow: hidden;
+	}
+
+	dialog {
+		display: grid;
+		background: var(--surface-2);
+		color: var(--text-1);
+		max-inline-size: min(90vw, var(--size-content-3));
+		max-block-size: min(80vh, 100%);
+		max-block-size: min(80dvb, 100%);
+		margin: auto;
+		padding: 0;
+		position: fixed;
+		inset: 0;
+		border-radius: var(--radius-3);
+		box-shadow: var(--shadow-6);
+		z-index: var(--layer-important);
+		overflow: hidden;
+		transition: opacity .5s var(--ease-3);
+	}
+	
+	@media (prefers-reduced-motion: no-preference) {
+		dialog {
+			animation: var(--animation-scale-down) forwards;
+			animation-timing-function: var(--ease-squish-3);
+		}
+	}
+	
+	@media (prefers-color-scheme: dark) {
+		dialog {
+			border-block-start: var(--border-size-1) solid var(--surface-3);
+		}
+	}
+	
+	@media (max-width: 768px) {
+		dialog[modal-mode="mega"] {
+				margin-block-end: 0;
+				border-end-end-radius: 0;
+				border-end-start-radius: 0;
+		}
+	}
+	
+	@media (max-width: 768px) and (prefers-reduced-motion: no-preference) {
+		dialog[modal-mode="mega"] {
+			animation: var(--animation-slide-out-down) forwards;
+			animation-timing-function: var(--ease-squish-2);
+		}
+	}
+	
+	@media (prefers-reduced-motion: no-preference) {
+		dialog:global([open]) {
+			animation: var(--animation-slide-in-up) forwards;
+		}
+	}
+
+	dialog:not([open]) {
+		pointer-events: none;
+		opacity: 0;
+	}
+
+	dialog[modal-mode="mega"]::backdrop {
+		backdrop-filter: blur(25px);
+	}
+
+	dialog[modal-mode="mini"]::backdrop {
+		backdrop-filter: none;
+	}
+
+	dialog::backdrop {
+		transition: backdrop-filter .5s ease;
+	}
+
+	dialog[loading] {
+		visibility: hidden;
+	}
+	
+	form {
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		align-items: start;
+		max-block-size: 80vh;
+		max-block-size: 80dvb;
+	}
+	
+	article {
+		overflow-y: scroll; 
+		max-block-size: 100%; /* safari */
+		overscroll-behavior-y: contain;
+		display: grid;
+		justify-items: flex-start;
+		gap: var(--size-3);
+		box-shadow: var(--shadow-2);
+		z-index: var(--layer-1);
+		padding-inline: var(--size-5) var(--size-3);
+		padding-block: var(--size-3);
+	}
+	
+	article::-webkit-scrollbar {
+		background: var(--surface-2);
+	}
+	
+	@media (prefers-color-scheme: light) {
+		article {
+			background: var(--surface-1);
+		}
+		
+		article::-webkit-scrollbar {
+			background: var(--surface-1);
+		}
+	}
+	
+	@media (prefers-color-scheme: dark) {
+		article {
+			border-block-start: var(--border-size-1) solid var(--surface-3);
+		}
+	}
+	
+	header {
+		display: flex;
+		gap: var(--size-3);
+		justify-content: space-between;
+		align-items: flex-start;
+		padding-block: var(--size-3);
+		padding-inline: var(--size-5);
+	}
+	
+	header > button {
+		border-radius: var(--radius-round);
+		padding: .75ch;
+		aspect-ratio: 1;
+		flex-shrink: 0;
+		place-items: center;
+		stroke: currentColor;
+		stroke-width: 3px;
+	}
+	
+	footer {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--size-3);
+		justify-content: space-between;
+		align-items: flex-start;
+		padding-inline: var(--size-5);
+		padding-block: var(--size-3);
+	}
+	
+	menu {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--size-3);
+		padding-inline-start: 0;
+	}
+	
+	menu:only-child {
+		margin-inline-start: auto;
+	}
+	
+
+	
+	:is(header, footer) {
+		background-color: var(--surface-2);
+	}
+	
+	@media (prefers-color-scheme: dark) {
+		:is(header, footer) {
+			background-color: var(--surface-1);
+		}
+	}
+</style>
